4b1b1b013a1cbd97b3da2dd996597f1b74fab51c
Xuqijun-FS-Code <KARBGAME@outlook.com>
2026-02-09 13:58:22 +0800

    Update import path for ApiResponse interface

--- backend/src/common/interceptors/transform.interceptor.ts
+++ backend/src/common/interceptors/transform.interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { ApiResponse } from '../api-response.interface';
+import { ApiResponse } from '../interceptors/api-response.interface';
 
 @Injectable()
 export class TransformInterceptor<T> implements NestInterceptor<T, ApiResponse<T>> {
